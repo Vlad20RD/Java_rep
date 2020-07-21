1f8a5317d72467f2c8efc404574eba513f4f1342
Vlad20IM <vlad-vlad-vlasov@inbox.ru>
2020-07-21 05:37:42 +0300

    Арифметические операции и конкатенация

--- sandbox/src/main/java/ru/stqa/pft/sandbox/hello.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/hello.java
@@ -3,5 +3,15 @@ package ru.stqa.pft.sandbox;
 public class hello {
 	public static void main(String[] args){
 		System.out.println("Hello, world!!!");
+		
+		System.out.println("(Сложение) 2+2 = " + (2 + 2));
+		System.out.println("(Вычитание) 2-2 = " + (2 - 2));
+		System.out.println("(Умножение) 2*2 = " + (2 * 2));
+		System.out.println("(Деление) 2/2 = " + (2 / 2));
+
+		System.out.println("(Деление с плавающей точкой) 3/7 = " + (3.0 / 7.0));
+
+		System.out.println("Соеди" + "нение" + " строки");
+		System.out.println("Конкатенация строки и числа: " + "2" + 2);
 	}
 }
