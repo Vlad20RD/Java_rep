1844f286ff3e8be2f371f85836a0b2e61e0bb705
Vladlen Vlasov <vlad-vlad-vlasov@inbox.ru>
2020-08-17 12:26:53 +0300

    42 формирование коллекции

--- addressbook2/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook2/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -3,21 +3,23 @@ import org.junit.Test;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 
 public class GroupDeletionTests extends TestBase{
 
   @Test
   public void groupDeletionTests() {
     app.getNavigationHelper().goToGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
     if(!app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("group1", null, null));
     }
-    app.getGroupHelper().selectGroup(before - 1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size());
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before - 1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
   }
 
 }
