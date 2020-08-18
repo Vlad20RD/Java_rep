298e792af62fde9f4bd2c7655733641e2c0b7c7d
Vladlen Vlasov <vlad-vlad-vlasov@inbox.ru>
2020-08-18 22:00:44 +0300

    44 неупорядоченные коллекции

--- addressbook2/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook2/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -68,7 +68,8 @@ public class GroupHelper extends HelperBase{
         List<WebElement> elements = driver.findElements(By.cssSelector("#content > form > input[type=checkbox]"));
         for(WebElement element : elements){
             String name = element.getText();
-            GroupData group = new GroupData(name, null, null);
+            String id = element.getAttribute("value");
+            GroupData group = new GroupData(id, name, null, null);
             groups.add(group);
         }
         return groups;
--- addressbook2/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook2/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.security.acl.Group;
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -17,10 +19,12 @@ public class GroupModificationTests extends TestBase{
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("name_mod", "header_mod", "footer_mod"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "group1", "group2", "group3");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
     }
 }
