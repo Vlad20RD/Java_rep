230d2aeead05b2e90749424cb2c58e753031c10c
Vladlen Vlasov <vlad-vlad-vlasov@inbox.ru>
2020-08-27 20:19:46 +0300

    исправление 30го урока

--- addressbook2/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook2/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,6 +3,9 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,9 +19,23 @@ public class ApplicationManager {
     private SessionHelper sessionHelper;
     JavascriptExecutor js;
     private Map<String, Object> vars;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        driver = new ChromeDriver();
+        if(browser.equals(BrowserType.FIREFOX)){
+            driver = new FirefoxDriver();
+        }
+        else if(browser.equals(BrowserType.CHROME)){
+            driver = new ChromeDriver();
+        }
+        else if(browser.equals(BrowserType.IE)){
+            driver = new InternetExplorerDriver();
+        }
+
         js = (JavascriptExecutor) driver;
         vars = new HashMap<String, Object>();
 
--- addressbook2/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook2/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase{
 
-    protected static final ApplicationManager app = new ApplicationManager();
+    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeSuite
     public void setUp() {
